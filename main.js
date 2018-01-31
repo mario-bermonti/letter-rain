dfb6f86fb97fe991036c5d485c273c7f3fde83f0
Mario E. Bermonti Pérez <mbermonti1132@gmail.com>
2018-01-31 11:15:16 -0400

    Create letter and move it

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
 enchant();
 window.onload = function() {
     var game = new Game(320, 320);
+
+    function moveLetters(letter){
+	//Move letters
+        letter.addEventListener("enterframe", function(){
+            this.y += 2;
+            this.frame = this.age % 2 + 16;
+        });
+
+	// remove letter from screen
+        //letter.addEventListener("touchstart", function(){
+            //game.rootScene.removeChild(letter);
+        //});
+}
+    function createLetters(){
+	//Letters
+        var letter = new Sprite(50, 50);
+        letter.image = game.assets["img/letters/a.png"];
+        letter.x = 20;
+        letter.y = 30;
+        letter.frame = 50;
+	game.rootScene.addChild(letter);
+	moveLetters(letter)
+    }
+
+    game.preload("img/letters/a.png");
     game.onload = function() {
         var label = new enchant.Label();
-        label.text = "Hello, enchant.js!";
+	//After calculating score, use it here
+        label.text = "Score: ";
         label.width = 128;
         label.height = 64;
-        label.font = "12px 'Arial'";
+	//Set the desired location
+	//Determine screen size
+        label.font = "20px 'Arial'";
         game.rootScene.addChild(label);
+
+	createLetters()
     }
     game.start();
 }
